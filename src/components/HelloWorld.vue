d9b145a8a76c75acd3c3e64806c4390642e7fcef
welang <fmorajara@gmail.com>
2024-06-22 21:17:46 -0400

    tailwind conf inicial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,7 +13,13 @@ defineProps({
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      <a
+        class="text-4xl font-bold underline"
+        href="https://vuejs.org/"
+        target="_blank"
+        rel="noopener"
+        >Vue 3</a
+      >.
     </h3>
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.vue',
+    'index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
